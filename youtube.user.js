42d2d0c0dd85841005621b39f2405a40843401f7
Kami Kaze <Kaze-Kami@users.noreply.github.com>
2025-05-30 01:56:13 +0200

    fix youtube script

--- youtube.user.js
+++ youtube.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Better youtube.com
-// @version     1.0
+// @version     1.2
 // @grant       none
 // @match       https://*.youtube.com/*
 // @run-at      document-start
@@ -8,32 +8,21 @@
 // ==/UserScript==
 
 // feed
-waitForElement(document.body, 'ytd-browse #primary', (element) => {
+waitForElement(document, '.ytd-browse #primary', (element) => {
   console.log('[SR] Content ready')
-  
-  const observer = new MutationObserver(() => {
-    console.log('[SR] Checking feed')
-    const shelfs = element.querySelectorAll('ytd-rich-section-renderer')
-    for (let shelf of shelfs) {
-      const title = shelf.querySelector('span#title').innerHTML
-      console.log(`[SR] Removing shelf ${title}`)
-      shelf.remove()
-    }
-  })
-  observer.observe(element, { childList: true, subtree: true })
+  waitForElement(element, '.ytd-rich-section-renderer', (e) => {
+    const title = e.querySelector('span#title').innerHTML
+    console.log(`[SR] Removing shelf ${title}`)
+    e.remove()
+  }, once = false)
 })
 
 // shorts in video recommendations
-waitForElement(document.body, '#items.ytd-watch-next-secondary-results-renderer', (items) => {
+waitForElement(document, '#items.ytd-watch-next-secondary-results-renderer', (items) => {
   console.log('[SR] Recommendations ready')
-  
-  const observer = new MutationObserver(() => {
-    console.log('[SR] Checking recommendations');
-    const shorts = items.querySelector('ytd-reel-shelf-renderer')
-    if (shorts) {
+
+  waitForElement(items, '.ytd-reel-shelf-renderer', (e) => {
       console.log(`[SR] Removing shorts in recommendations`)
-      shorts.remove()
-    }
-  })
-  observer.observe(items, { childList: true, subtree: true })
+      e.remove()
+  }, once = false);
 })
